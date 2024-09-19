e7377f42023377e51dec407804b0fca61a19a786
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 10:42:05 +0200

    Saved in TypeScript Playground at 2024-09-19T08:42:04.966Z

---
+++ arcs/index.ts
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+
+  //we want to use degrees, not rad. Want to learn more?
+  //https://brilliant.org/wiki/degrees-radian/
+    angleMode(DEGREES);
+
+    stroke("black")
+    strokeWeight(10);
+    fill("yellow");
+
+    circle(200, 200, 375);
+
+    fill("black");
+    circle(125, 125, 20);
+    circle(275, 125, 20);
+
+    noFill();
+
+
+}
