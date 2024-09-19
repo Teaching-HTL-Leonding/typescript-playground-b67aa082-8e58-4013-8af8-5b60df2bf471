e9570ad6f0805f0dd26dc12e30e0d61add9909c3
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 18:17:20 +0200

    Saved in TypeScript Playground at 2024-09-19T16:17:20.622Z

--- cat/index.ts
+++ cat/index.ts
@@ -1,24 +1,25 @@
-function setup() {createCanvas(500, 500);
+function setup() {
+    createCanvas(500, 500);
     strokeWeight(2);
-     background("skyblue");
-      fill("lightgrey"); 
-      circle(250, 250, 300,); 
-      fill("black");
-      circle(200, 230, 30);
-      circle(300, 230, 30);
-      fill("lightgrey");
-      triangle(140,60,120,150, 180, 100);
-      triangle(325,50,300,100, 375, 135);
-      fill("pink")
-      triangle(235,250,265,250,250,270)
-        line(165,250,235,275)
-        line(165,285,235,285)
-        line(265,285,340,285)
-        line(265,275,340,245)
-        line(250,270,250,300)
-line(250,300,200,315)
-line(250,300,300,315)
-line(200,315,165,300)
-line(300,315,340,300)
+    background("aqua");
+    fill("lightgrey");
+    circle(250, 250, 300,);
+    fill("black");
+    circle(200, 230, 30);
+    circle(300, 230, 30);
+    fill("lightgrey");
+    triangle(140, 60, 120, 150, 180, 100);
+    triangle(325, 50, 300, 100, 375, 135);
+    fill("pink")
+    triangle(235, 250, 265, 250, 250, 260)
+    line(165, 250, 235, 275)
+    line(165, 285, 235, 285)
+    line(265, 285, 340, 285)
+    line(265, 275, 340, 245)
+    line(250, 260, 250, 300)
+    line(250, 300, 235, 312)
+    line(250, 300, 265, 312)
+    line(235, 312, 165, 300)
+    line(265, 312, 340, 300)
 }
-}
+
