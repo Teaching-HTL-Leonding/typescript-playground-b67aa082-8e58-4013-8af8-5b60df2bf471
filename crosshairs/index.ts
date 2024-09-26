b06e0e1e9d92975993f1f9f6685fe4ce9b5e346f
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 08:37:52 +0200

    Saved in TypeScript Playground at 2024-09-26T06:37:52.131Z

---
+++ crosshairs/index.ts
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+}
+ 
+
+
+function mouseMoved() {
+  strokeWeight(2)
+  background("lightblue");
+  noFill();
+  text(`X: ${mouseX}, Y: ${mouseY}`, 5, height - 5);
+circle(mouseX, mouseY, 50);
+circle(mouseX, mouseY, 25)
+  fill("black");
+  rect(mouseX , mouseY - 35, 2, 75)
+ rect(mouseX -50, mouseY, 75, 2)
+}
