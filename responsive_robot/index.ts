2738433ee51579bf93a129db43f7dc4b10ac2bff
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 10:13:53 +0200

    Saved in TypeScript Playground at 2024-09-26T08:13:53.111Z

--- responsive_robot/index.ts
+++ responsive_robot/index.ts
@@ -19,6 +19,8 @@ function setup() {
   const lineY1 = height/2 -headHeight/2
   const lineX2 = width/2
   const lineY2 = height/2 -headHeight *0.75
+  const antenneX = width/2
+  const antenneY = height/2 - headHeight*0.75
   strokeWeight(2);
   fill("gray");
   rect(headX, headY, headWidth, headHeight);
@@ -28,6 +30,7 @@ function setup() {
   circle(eyeX1, eyeY1, 40);
   circle(eyeX2, eyeY2, 40);
   line(lineX1, lineY1, lineX2, lineY2);
-  // Add your code here
 
+  fill("white");
+  circle(antenneX,antenneY,20)
 }
