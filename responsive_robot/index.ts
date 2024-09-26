c3c3b45af55548ee11d9e3210956c2673e5ff7d2
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 10:18:03 +0200

    Saved in TypeScript Playground at 2024-09-26T08:18:02.887Z

--- responsive_robot/index.ts
+++ responsive_robot/index.ts
@@ -2,9 +2,9 @@ function setup() {
   createCanvas(500, 500);
   background("lightgray");
 
-  const headWidth = width * 0.50; 
-  const headHeight = height * 0.50; 
-  const headX = width / 2 - headWidth / 2; 
+  const headWidth = width * 0.50;
+  const headHeight = height * 0.50;
+  const headX = width / 2 - headWidth / 2;
   const headY = height / 2 - headHeight / 2;
   const mouthWidth = headWidth * 0.50;
   const mouthHeight = headHeight * 0.1;
@@ -14,12 +14,12 @@ function setup() {
   const eyeY1 = height / 2 - headHeight * 0.25;
   const eyeX2 = width / 2 + headWidth * 0.25;
   const eyeY2 = height / 2 - headHeight * 0.25;
-  const lineX1 = width/2;
-  const lineY1 = height/2 -headHeight/2;
-  const lineX2 = width/2;
-  const lineY2 = height/2 -headHeight *0.75;
-  const antenneX = width/2;
-  const antenneY = height/2 - headHeight*0.75;
+  const lineX1 = width / 2;
+  const lineY1 = height / 2 - headHeight / 2;
+  const lineX2 = width / 2;
+  const lineY2 = height / 2 - headHeight * 0.75;
+  const antenneX = width / 2;
+  const antenneY = height / 2 - headHeight * 0.75;
   strokeWeight(2);
   fill("gray");
   rect(headX, headY, headWidth, headHeight);
@@ -30,5 +30,5 @@ function setup() {
   circle(eyeX2, eyeY2, 40);
   line(lineX1, lineY1, lineX2, lineY2);
   fill("white");
-  circle(antenneX,antenneY,20);
+  circle(antenneX, antenneY, 20);
 }
