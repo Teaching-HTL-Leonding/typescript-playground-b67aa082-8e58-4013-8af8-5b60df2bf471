3f96df01eb921e96e107171e04b4a5c9d555bef5
Rainer Stropek <rainer@timecockpit.com>
2024-09-28 15:45:04 +0200

    Saved in TypeScript Playground at 2024-09-28T13:45:03.617Z

--- string_concat/index.ts
+++ string_concat/index.ts
@@ -1,26 +1,23 @@
+let clickProtocol = "0/0";
 
-let clickprotocol: string = "0/0"
 function setup() {
   createCanvas(400, 400);
   background("lightblue");
-  noStroke()
-  fill("aqua")
-  circle(0, 0, 20)
-}
-
-
+  noStroke();
+  fill("aqua");
+  circle(0,0, 20)
+  
+} 
 
 function mouseClicked() {
-  clickprotocol += `,${mouseX}/${mouseY}`;
-
-  fill("lightblue");
-  rect(0, 0, 400, 400)
-  noStroke()
+fill("lightblue")
+  rect(0,0, 400, 400)
+  noStroke();
   fill("aqua");
-  circle(mouseX, mouseY, 20,);
-  fill("black")
-  text(`${clickprotocol}`, 0, 0, 400, 400);
+  circle(mouseX, mouseY, 20);
 
+  clickProtocol += `, ${mouseX}/${mouseY}`;
 
+  fill("black");
+  text(clickProtocol , 0, 0, 400, 400);
 }
-
