7811775df4f6405a9bb43f235f2c0a897ad86807
Rainer Stropek <rainer@timecockpit.com>
2024-09-28 15:45:59 +0200

    Saved in TypeScript Playground at 2024-09-28T13:45:59.227Z

--- string_concat/index.ts
+++ string_concat/index.ts
@@ -5,19 +5,16 @@ function setup() {
   background("lightblue");
   noStroke();
   fill("aqua");
-  circle(0,0, 20)
-  
-} 
+  circle(0, 0, 20)
+}
 
 function mouseClicked() {
-fill("lightblue")
-  rect(0,0, 400, 400)
+  fill("lightblue")
+  rect(0, 0, 400, 400)
   noStroke();
   fill("aqua");
   circle(mouseX, mouseY, 20);
-
   clickprotocol += `, ${mouseX}/${mouseY}`;
-
   fill("black");
-  text(clickprotocol , 0, 0, 400, 400);
+  text(clickprotocol, 0, 0, 400, 400);
 }
