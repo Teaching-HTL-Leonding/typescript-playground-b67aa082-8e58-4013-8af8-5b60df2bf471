b1fc4753dfd2761fea116e98ae369dc14a41b2af
Rainer Stropek <rainer@timecockpit.com>
2024-10-09 18:00:11 +0200

    Saved in TypeScript Playground at 2024-10-09T16:00:11.030Z

--- bouncing_ball_advanced/index.ts
+++ bouncing_ball_advanced/index.ts
@@ -20,11 +20,11 @@ function draw() {
   circleCenterX += xdirection;
   circleCenterY += ydirection;
 
-  if (circleCenterX >= width || circleCenterX <= 0 ) {
+  if (circleCenterX >= width || circleCenterX <= 0) {
     xdirection *= -1;
   }
-  if( circleCenterY >=height || circleCenterY <= 0){
-        ydirection *= -1;
+  if (circleCenterY >= height || circleCenterY <= 0) {
+    ydirection *= -1;
 
   }
 }
