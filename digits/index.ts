a35e34d0fe6c95ded63b76656966bb01101429bc
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 10:10:05 +0200

    Saved in TypeScript Playground at 2024-10-10T08:10:04.744Z

--- digits/index.ts
+++ digits/index.ts
@@ -1,4 +1,5 @@
 function setup() {
+  createCanvas(1000,1000)
   const num = Math.floor(random(0, 1_000_000));
   const lastDigit1 = num % 10;
   const numWithoutLastDigit = Math.floor(num / 10);
@@ -17,5 +18,14 @@ function setup() {
   textAlign(CENTER, CENTER);
   fill("yellow");
   noStroke();
-  text(num, width / 2, height - 20);
+  text(num, width / 4, height - 20);
+  text(lastDigit1, width/4 + 100, height-20)
+  text(lastDigit2, width/4 + 200, height-20)
+  text(lastDigit3, width/4 + 300, height-20)
+  text(lastDigit4, width/4 + 400, height-20)
+  text(lastDigit5, width/4 + 500, height-20)
+  text(lastDigit6, width/4 + 600, height-20)
+
+
+
 }
