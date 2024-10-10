9785341542892a8ea439ad8e219cb960da43da63
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 10:27:59 +0200

    Saved in TypeScript Playground at 2024-10-10T08:27:59.022Z

--- digits/index.ts
+++ digits/index.ts
@@ -19,13 +19,13 @@ function setup() {
   textAlign(CENTER, CENTER);
   fill("yellow");
   noStroke();
-  text(num, width / 4, height - 20);
-  text(lastDigit1, width/4 + 100, height-20)
-  text(lastDigit2, width/4 + 200, height-20)
-  text(lastDigit3, width/4 + 300, height-20)
-  text(lastDigit4, width/4 + 400, height-20)
-  text(lastDigit5, width/4 + 500, height-20)
-  text(lastDigit6, width/4 + 600, height-20)
+  text(num, width / 2, height - 20);
+  text(lastDigit1, width-width/3+width/25, height-height/4*3)
+  text(lastDigit2, width/3*2-width/12, height-height/4*3)
+  text(lastDigit3,width/2-width/24 , height-height/4*3)
+  text(lastDigit4, width/2-width/6 , height-height/4*3)
+  text(lastDigit5, width/5 , height-height/4*3)
+  text(lastDigit6, width/15, height-height/4*3)
   noFill();
   stroke("yellow")
   
@@ -34,7 +34,7 @@ function setup() {
   rect(width/2-width/6,height-height/4*3,width/15,height/4)
   rect(width/2-width/24,height-height/4*3,width/15,height/4)
   rect(width/3*2-width/12,height-height/4*3,width/15,height/4)
-    rect(width-width/3*2,height-height/4*3,width/15,height/4)
+  rect(width-width/3+width/25,height-height/4*3,width/15,height/4)
 
 
 
