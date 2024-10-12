1ac6f609fee3199a03d8a2871b7f50216c2f7b0d
Rainer Stropek <rainer@timecockpit.com>
2024-10-12 10:07:38 +0200

    Saved in TypeScript Playground at 2024-10-12T08:07:38.491Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -8,90 +8,70 @@ function setup() {
   const numWithoutLastDigit1 = Math.floor(numWithoutLastDigit / 16);
   const lastDigit3 = numWithoutLastDigit1 % 16;
   fill("yellow");
-  textSize(80)
+  textSize(60)
   textAlign(CENTER, CENTER);
   if (lastDigit1 < 10) {
-    text(lastDigit1, width - width / 3 + width / 25 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit1 === 10) {
-    text("A", width - width / 3 + width / 25 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit1 === 11) {
-    text("B", width - width / 3 + width / 25 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit1 === 12) {
-    text("C", width - width / 3 + width / 25 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit1 === 13) {
-    text("D", width - width / 3 + width / 25 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit1 === 14) {
-    text("E", width - width / 3 + width / 25 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit1 === 15) {
-    text("F", width - width / 3 + width / 25 + width / 27, height - height / 4 * 3 + height / 7)
+    text(lastDigit1, width / 2 + width / 8, height / 2 - 80, 100, 160)
+  } if (lastDigit1 === 10) {
+    text("A", width / 2 + width / 8, height / 2 - 80, 100, 160)
+  } if (lastDigit1 === 11) {
+    text("B", width / 2 + width / 8, height / 2 - 80, 100, 160)
+  } if (lastDigit1 === 12) {
+    text("C", width / 2 + width / 8, height / 2 - 80, 100, 160)
+  } if (lastDigit1 === 13) {
+    text("D", width / 2 + width / 8, height / 2 - 80, 100, 160)
+  } if (lastDigit1 === 14) {
+    text("E", width / 2 + width / 8, height / 2 - 80, 100, 160)
+  } if (lastDigit1 === 15) {
+    text("F", width / 2 + width / 8, height / 2 - 80, 100, 160)
   }
 
   if (lastDigit2 < 10) {
-    text(lastDigit2, width / 3 * 2 - width / 12 + width / 27, height - height / 4 * 3 + height / 7)
+    text(lastDigit2, width / 2 , height / 2 )
+  } if (lastDigit2 === 10) {
+    text("A", width / 2 , height / 2 )
+  } if (lastDigit2 === 11) {
+    text("B", width / 2 , height / 2 )
+  } if (lastDigit2 === 12) {
+    text("C", width / 2 , height / 2 )
+  } if (lastDigit2 === 13) {
+    text("D", width / 2 , height / 2 )
+  } if (lastDigit2 === 14) {
+    text("E", width / 2 , height / 2 )
+  } if (lastDigit2 === 15) {
+    text("F", width / 2 , height / 2 )
+  }
 
-  }
-  if (lastDigit2 === 10) {
-    text("A", width / 3 * 2 - width / 12 + width / 27, height - height / 4 * 3 + height / 7)
-
-  }
-  if (lastDigit2 === 11) {
-    text("B", width / 3 * 2 - width / 12 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit2 === 12) {
-    text("C", width / 3 * 2 - width / 12 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit2 === 13) {
-    text("D", width / 3 * 2 - width / 12 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit2 === 14) {
-    text("E", width / 3 * 2 - width / 12 + width / 27, height - height / 4 * 3 + height / 7)
-  }
-  if (lastDigit2 === 15) {
-    text("F", width / 3 * 2 - width / 12 + width / 27, height - height / 4 * 3 + height / 7)
-  }
   if (lastDigit3 < 10) {
-    text(lastDigit3, width / 2 - width / 24 + width / 27, height - height / 4 * 3 + height / 7)
+    text(lastDigit3, width / 2 - width / 4, height / 2 - 80, 100, 160)
   }
   if (lastDigit3 === 10) {
-    text("A", width / 2 - width / 24 + width / 27, height - height / 4 * 3 + height / 7)
+    text("A", width / 2 - width / 4, height / 2 - 80, 100, 160)
   }
   if (lastDigit3 === 11) {
-    text("B", width / 2 - width / 24 + width / 27, height - height / 4 * 3 + height / 7)
+    text("B", width / 2 - width / 4, height / 2 - 80, 100, 160)
   }
   if (lastDigit3 === 12) {
-    text("C", width / 2 - width / 24 + width / 27, height - height / 4 * 3 + height / 7)
+    text("C", width / 2 - width / 4, height / 2 - 80, 100, 160)
   }
   if (lastDigit3 === 13) {
-    text("D", width / 2 - width / 24 + width / 27, height - height / 4 * 3 + height / 7)
+    text("D", width / 2 - width / 4, height / 2 - 80, 100, 160)
   }
   if (lastDigit3 === 14) {
-    text("E", width / 2 - width / 24 + width / 27, height - height / 4 * 3 + height / 7)
+    text("E", width / 2 - width / 4, height / 2 - 80, 100, 160)
   }
   if (lastDigit3 === 15) {
-    text("F", width / 2 - width / 24 + width / 27, height - height / 4 * 3 + height / 7)
+    text("F", width / 2 - width / 4, height / 2 - 80, 100, 160)
   }
 
-
-
   noStroke();
-  text(lastDigit3, width / 2 - width / 24 + width / 27, height - height / 4 * 3 + height / 7)
-
   textSize(20)
   text(num, width / 2, height - 20);
-
   noFill();
   stroke("yellow")
-
-
-  rect(width / 2 - width / 24, height - height / 4 * 3, width / 15, height / 4)
-  rect(width / 3 * 2 - width / 12, height - height / 4 * 3, width / 15, height / 4)
-  rect(width - width / 3 + width / 25, height - height / 4 * 3, width / 15, height / 4)
+  rect(width / 2 - width / 4, height / 2 - 80, 100, 160)
+  rect(width / 2 - 50, height / 2 - 80, 100, 160)
+  rect(width / 2 + width / 8, height / 2 - 80, 100, 160)
 
 
 }
