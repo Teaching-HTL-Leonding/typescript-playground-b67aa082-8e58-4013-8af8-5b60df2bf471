3e6f6d47391ea7e5bbe1abcf4fa043264f1a7b61
Rainer Stropek <rainer@timecockpit.com>
2024-10-22 13:32:43 +0200

    Saved in TypeScript Playground at 2024-10-22T11:32:43.134Z

--- rps_part_2/index.ts
+++ rps_part_2/index.ts
@@ -1,7 +1,4 @@
-// We declare constants to avoid repeating "magic numbers". 
-// These numbers are used for positioning text and icons on the canvas.
-// Note that we use all-caps for constants that represent 
-// configuration values to make them easy to recognize.
+
 const TEXT_LEFT = 30;
 const STONE_LEFT = 50;
 const PAPER_LEFT = 175;
@@ -19,7 +16,7 @@ function setup() {
     textSize(30);
     text("Human:", TEXT_LEFT, 50);
 
-    // Display the icons for "stone", "paper", and "scissors".
+    
     textSize(75);
     text("🪨", STONE_LEFT, HUMAN_SYMBOLS_TOP);
     text("📃", PAPER_LEFT, HUMAN_SYMBOLS_TOP);
@@ -27,26 +24,22 @@ function setup() {
 }
 
 function mouseMoved() {
-    // This function highlights the icon currently being hovered over by the mouse.
-    // <<< Add code here to disable the hover effect once the user has made a choice
+    
     noFill();
     strokeWeight(3);
 
-    // In the following lines of code, we repeatedly check if the mouse
-    // is vertically in the relevant area. Therefore, we calculate the boolean
-    // value once and use the variable later multiple times.
+    
     const isInVertical = mouseY >= ICON_TOP && mouseY < ICON_TOP + ICON_HEIGHT;
 
     if(computerv2 === 0){
     if (isInVertical && mouseX >= STONE_LEFT && mouseX < STONE_LEFT + ICON_WIDTH) {
-        stroke("yellow"); // Highlight with yellow.
+        stroke("yellow"); 
     } else {
-        stroke("black"); // Remove highlight by drawing in black.
+        stroke("black"); 
     }
-    // Draw a rectangle around the "stone" icon.
     rect(STONE_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT);
 
-    // Same for paper
+ 
     if (isInVertical && mouseX >= PAPER_LEFT && mouseX < PAPER_LEFT + ICON_WIDTH) {
         stroke("yellow");
     } else {
@@ -54,7 +47,7 @@ function mouseMoved() {
     }
     rect(PAPER_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT);
 
-    // Same for scissors
+  
     if (isInVertical && mouseX >= SCISSORS_LEFT && mouseX < SCISSORS_LEFT + ICON_WIDTH) {
         stroke("yellow");
     } else {
