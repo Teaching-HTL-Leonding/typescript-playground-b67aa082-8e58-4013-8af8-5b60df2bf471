b7345f866c1cddd56ebf661565597795704207d3
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 09:58:51 +0200

    Saved in TypeScript Playground at 2024-10-24T07:58:50.618Z

--- calculator_if/index.ts
+++ calculator_if/index.ts
@@ -8,7 +8,15 @@ function setup() {
     createCanvas(300, 500);
     background("lightgray");
     textSize(50)
+    textAlign(CENTER)
     text(`0`,width/3-12,height-20)
+    text(`C`,width-75,height-20)
+    text(`1`,width/3-60,height/5*4-20)
+    text(`2`,width/3*2-60,height/5*4-20)
+    text(`3`,width-60,height/5*4-20)
+    text(`4`,width/3-60,height/5*3-20)
+    text(`7`,width/3-60,height/5*2-20)
+    text(`5`,width/3*2-60,height/5*3-20)
     strokeWeight(3)
     line(0,height/5,width,height/5)
     line(0,height/5*2,width,height/5*2)
