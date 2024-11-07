5fafe28021bbfbc78a7e41fa1e46c06d6cab2cc3
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 09:51:27 +0100

    Saved in TypeScript Playground at 2024-11-07T08:51:27.204Z

--- math_trainer_exam/index.ts
+++ math_trainer_exam/index.ts
@@ -5,7 +5,7 @@
 let one = 0
 let two = 0
 let three = 0
-let  position = 0
+let position = 0
 let operandnumber = 0
 let operand = ""
 let one1 = 0
@@ -14,13 +14,13 @@ let rightanswer = 0
 function setup() {
     stroke("yellow")
     noFill()
-    rect(width/2-75,height/2-75,75,75)
+    rect(width / 2 - 75, height / 2 - 75, 75, 75)
     one = Math.floor(random(1, 101))
     two = Math.floor(random(1, 101))
     three = Math.floor(random(1, 101))
     position = Math.floor(random(1, 4))
     operandnumber = Math.floor(random(1, 4))
-    
+
     if (operandnumber === 1) {
         operand = "+"
     }
@@ -28,15 +28,19 @@ function setup() {
         operand = "-"
     }
     else if (operandnumber === 3) {
-        operand = "*"}
-    if(operand ==="+"||operand === " -"){
+        operand = "*"
+    }
+    if (operand === "+" || operand === " -") {
         one1 = Math.floor(random(1, 101))
-        two1 = Math.floor(random(1, 101))}
-    else{ one1 = Math.floor(random(1, 11))
-        two1 = Math.floor(random(1, 11))}
-    if(operand ==="-"&&one1<two1){
-        one1=two1;
-        two1= Math.floor(random(0,one1))
+        two1 = Math.floor(random(1, 101))
+    }
+    else {
+        one1 = Math.floor(random(1, 11))
+        two1 = Math.floor(random(1, 11))
+    }
+    if (operand === "-" && one1 < two1) {
+        one1 = two1;
+        two1 = Math.floor(random(0, one1))
     }
 
     if (operand === "+") {
@@ -52,29 +56,29 @@ function setup() {
     background("black");
 
 
-    
+
     textSize(40)
     stroke("yellow")
     fill("yellow")
     if (position === 1) {
-        text(`${rightanswer}`, width / 3-width/5, height / 2)
+        text(`${rightanswer}`, width / 3 - width / 5, height / 2)
     }
     if (position === 2) {
-        text(`${rightanswer}`, width/ 2-10, height / 2)
+        text(`${rightanswer}`, width / 2 - 10, height / 2)
     }
     if (position === 3) {
-        text(`${rightanswer}`, width / 3 * 2+width/8, height / 2)
+        text(`${rightanswer}`, width / 3 * 2 + width / 8, height / 2)
     }
-    if(position != 1 ){
-        text(`${one}`, width / 3-width/5, height / 2)
+    if (position != 1) {
+        text(`${one}`, width / 3 - width / 5, height / 2)
     }
-    if(position != 2){
-            text(`${two}`, width/ 2 -width/40, height / 2)
+    if (position != 2) {
+        text(`${two}`, width / 2 - width / 40, height / 2)
     }
-    if(position != 3){
-        text(`${three}`, width /3*2+width/8, height / 2)
+    if (position != 3) {
+        text(`${three}`, width / 3 * 2 + width / 8, height / 2)
     }
-    
+
     textSize(40)
     stroke("yellow")
     fill("yellow")
@@ -89,28 +93,31 @@ function setup() {
 function mouseClicked() {
     fill("black")
     noStroke()
-    rect(0,height/3*2,width,height)
+    rect(0, height / 3 * 2, width, height)
     textSize(40)
     stroke("darkgreen")
     fill("darkgreen")
     textAlign(LEFT)
-    if( position === 1 && mouseX > 0 && mouseX<width /3&& mouseY>height/2-height/10&&mouseY<height/2 ){
-        text(`${rightanswer} ist richtig!`,width/8,height/8*7)
+    if (position === 1 && mouseX > 0 && mouseX < width / 3 && mouseY > height / 2 - height / 10 && mouseY < height / 2) {
+        text(`${rightanswer} ist richtig!`, width / 8, height / 8 * 7)
     }
-    if( position === 2 && mouseX > width/3 && mouseX<width /3*2&& mouseY>height/2-height/10&&mouseY<height/2 ){
-        text(`${rightanswer} ist richtig!`,width/8,height/8*7)
+    if (position === 2 && mouseX > width / 3 && mouseX < width / 3 * 2 && mouseY > height / 2 - height / 10 && mouseY < height / 2) {
+        text(`${rightanswer} ist richtig!`, width / 8, height / 8 * 7)
     }
-    if( position === 3 && mouseX > width/3*2 && mouseX<width /3*3&& mouseY>height/2-height/10&&mouseY<height/2 ){
-        text(`${rightanswer} ist richtig!`,width/8,height/8*7)}
-         fill("red")
-         stroke("red")
-    if(position != 1&& mouseX> 0 && mouseX<width/3&& mouseY>height/2-height/10&&mouseY<height/2){
-        text(`${one} ist falsch!`,width/8,height/8*7)
+    if (position === 3 && mouseX > width / 3 * 2 && mouseX < width / 3 * 3 && mouseY > height / 2 - height / 10 && mouseY < height / 2) {
+        text(`${rightanswer} ist richtig!`, width / 8, height / 8 * 7)
     }
- if( position != 2 && mouseX > width/3 && mouseX<width /3*2&& mouseY>height/2-height/10&&mouseY<height/2){
-        text(`${two} ist falsch!`,width/8,height/8*7)}
- if( position != 3 && mouseX > width/3*2 && mouseX<width /3*3&& mouseY>height/2-height/10&&mouseY<height/2 ){
-        text(`${three} ist falsch!`,width/8,height/8*7)}
+    fill("red")
+    stroke("red")
+    if (position != 1 && mouseX > 0 && mouseX < width / 3 && mouseY > height / 2 - height / 10 && mouseY < height / 2) {
+        text(`${one} ist falsch!`, width / 8, height / 8 * 7)
+    }
+    if (position != 2 && mouseX > width / 3 && mouseX < width / 3 * 2 && mouseY > height / 2 - height / 10 && mouseY < height / 2) {
+        text(`${two} ist falsch!`, width / 8, height / 8 * 7)
+    }
+    if (position != 3 && mouseX > width / 3 * 2 && mouseX < width / 3 * 3 && mouseY > height / 2 - height / 10 && mouseY < height / 2) {
+        text(`${three} ist falsch!`, width / 8, height / 8 * 7)
+    }
 
 }
 // mouseMoved function displays mouse coordinates as feedback
