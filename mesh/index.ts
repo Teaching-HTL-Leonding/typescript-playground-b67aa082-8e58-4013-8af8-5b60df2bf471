769d8c03785d3f854f55da5b94d699aa16cb3a4c
Rainer Stropek <rainer@timecockpit.com>
2024-11-14 10:47:43 +0100

    Saved in TypeScript Playground at 2024-11-14T09:47:43.560Z

--- mesh/index.ts
+++ mesh/index.ts
@@ -1,17 +1,32 @@
 const SIZE = 400;  // Canvas size (square)
 const MARGIN = 50; // Margin between the edges and the rays.
-                  // This is also the distance between the rays.
-let u = 0
+                   // This is also the distance between the rays.
+
+// Use the HSB color scheme for this exercise. Draw the first rays
+// (from left-upper corner) with hue 0 (=red), the next rays with
+// hue 60 (yellow), the next with 120 (green), etc. Use the following
+// constant for incrementing the hue value from point to point.
+
+
 function setup() {
   createCanvas(SIZE, SIZE);
   background("black");
 
   strokeWeight(1);
-  stroke("lime");
-while(u<3){
-let i = 0
-  while(i<3){
-    line(MARGIN,i,SIZE-MARGIN,MARGIN)
-    i+= MARGIN
-  }}
+  colorMode(HSB);
+
+  let i = MARGIN;
+  
+  while (i <= SIZE - MARGIN) {
+   
+    let u = MARGIN;
+    while (u <= SIZE - MARGIN) {
+        stroke("green")
+      line(MARGIN, i, width - MARGIN, u);
+      u += MARGIN;
+    }
+
+    i += MARGIN;
+   
+  }
 }
