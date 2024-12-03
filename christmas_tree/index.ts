a0fb8685c78fbb5f0f74e8c9bf4d3e480c440b34
Rainer Stropek <rainer@timecockpit.com>
2024-12-03 09:45:20 +0100

    Saved in TypeScript Playground at 2024-12-03T08:45:19.729Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -1,13 +1,6 @@
 const TOP_MARGIN = 80;
 const SPHERE_DIAMETER = 30;
-const WIDTH1 = 100;
-const HEIGHT1 = WIDTH1*0.5
-const WIDTH2 = WIDTH1*1.5
-const HEIGHT2 = WIDTH2*0.5
-const WIDTH3 = WIDTH2 * 1.5
-const HEIGHT3 = WIDTH3 * 0.5
-const WIDTH4 = WIDTH3 * 1.5
-const HEIGHT4 = WIDTH4 * 0.5
+const INITIAL_TRIANGLE_BASE_WIDTH = 100;
 const SIZE_INCREASE_BY_LAYER = 0.5;
 const HEIGHT_TO_WIDTH_RATIO = 0.5;
 const NUMBER_OF_LAYERS = 4;
@@ -15,10 +8,28 @@ const TRUNK_WIDTH = 50;
 const TRUNK_HEIGHT = 50;
 
 function setup() {
+  let color = Math.floor(random(0,360))
+  
   createCanvas(500, 500);
   background("black");
-  fill("green");
   noStroke();
-triangle(width/2,TOP_MARGIN,width/2-WIDTH1/2,TOP_MARGIN+HEIGHT1,width/2+WIDTH1/2,TOP_MARGIN+HEIGHT1)
-triangle(width/2,TOP_MARGIN+HEIGHT1/2,width/2-WIDTH2/2,TOP_MARGIN+HEIGHT2,width/2+WIDTH2/2,TOP_MARGIN+HEIGHT2)
+  fill("brown")
+  resetMatrix
+  rect (width/2-TRUNK_WIDTH/2,height/4.08*3,TRUNK_WIDTH,TRUNK_HEIGHT)
+  let trianglewidth = INITIAL_TRIANGLE_BASE_WIDTH
+  translate(width/2,TOP_MARGIN)
+  for(let i = 0;i<NUMBER_OF_LAYERS;i++){
+  fill("green")
+    triangle(-trianglewidth/2,trianglewidth/2,0,0,trianglewidth/2,trianglewidth/2)
+  colorMode(HSB)
+   fill(color,50,100)
+    circle(-trianglewidth/2,trianglewidth/2+15,SPHERE_DIAMETER)
+    circle(trianglewidth/2,trianglewidth/2+15,SPHERE_DIAMETER)
+    translate(0,trianglewidth/4)
+    trianglewidth *= 1.5
+
+    color += 60
+
+
+  }
 }
