f10e61be883e8605c7c8478caac341c23db9717d
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 08:44:21 +0100

    Saved in TypeScript Playground at 2024-12-05T07:44:21.181Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -12,15 +12,23 @@ function setup() {
 
 function mouseMoved() {
     resetMatrix();
+    let l = Math.round(mouseX/601*100)
+    
     background("black");
     strokeWeight(2);
+    noStroke()
+    fill("black")
+    rect(0,285,width,16)
+    fill("white")
+    text(`${mouseX} of 601 = ${l}%`,47,295)
 
+    textSize(12)
     noFill();
     stroke("aqua");
     let height1 = 0
     let height2 = 5
     for (let x = 0; x < 601; x++) {
-        for (let i = 0; i < 50; i += 1) {
+        for (let i = 0; i < 29; i += 1) {
             line(0, height1, 10, height2)
             height1 += 10
             line(10,height2,0,height1)
@@ -30,7 +38,5 @@ function mouseMoved() {
         height1 = 0
         height2 = 5
     }
-
-
 
 }
