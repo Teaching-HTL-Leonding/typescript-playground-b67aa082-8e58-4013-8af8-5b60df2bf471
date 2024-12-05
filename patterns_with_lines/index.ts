1fd33767332381bca84c752de32071d402dbd4df
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 08:57:46 +0100

    Saved in TypeScript Playground at 2024-12-05T07:57:46.468Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -12,15 +12,15 @@ function setup() {
 
 function mouseMoved() {
     resetMatrix();
-    let l = Math.round(mouseX/601*100)
-    
+    let l = Math.round(mouseX / 601 * 100)
+
     background("black");
     strokeWeight(2);
     noStroke()
     fill("black")
-    rect(0,285,width,16)
+    rect(0, 285, width, 16)
     fill("white")
-    text(`${mouseX} of 601 = ${l}%`,47,296)
+    text(`${mouseX} of 601 = ${l}%`, 47, 296)
     colorMode(HSB)
     textSize(12)
     noFill();
@@ -28,13 +28,13 @@ function mouseMoved() {
     let height2 = 5
     let color = 0
     for (let x = 0; x < 601; x++) {
-    stroke(color,100,100);
+        stroke(color, 100, 100);
         for (let i = 0; i < 29; i += 1) {
             line(0, height1, 10, height2)
             height1 += 10
-            line(10,height2,0,height1)
+            line(10, height2, 0, height1)
             height2 += 10
-        color = (color+0.2)%360
+            color = (color + 0.2) % 360
         }
         translate(10, 0)
         height1 = 0
