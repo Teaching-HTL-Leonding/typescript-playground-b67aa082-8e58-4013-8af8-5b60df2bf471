d18012bd675c52b7e35174014b974478462dd1ca
Rainer Stropek <rainer@timecockpit.com>
2024-12-19 08:17:36 +0100

    Saved in TypeScript Playground at 2024-12-19T07:17:35.566Z

---
+++ scaled_circles/index.ts
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(400, 400);
+    background("white");
+    noFill();
+
+    stroke("blue");
+    strokeWeight(3);
+    circle(50,50,50);
+    push();
+
+    stroke("red")
+    scale(2)
+    strokeWeight(3/2)
+    circle(50,50,50)
+
+    pop()
+    push();
+    stroke("green");
+    scale(4)
+    strokeWeight(3/4)
+    circle(50,50,50)
+    pop();
+    // <<< Add your code here
+}
