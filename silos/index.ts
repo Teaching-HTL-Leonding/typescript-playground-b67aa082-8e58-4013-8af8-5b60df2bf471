f2d97522bc8dc101939ecb6c4d38eac15481a9c1
Rainer Stropek <rainer@timecockpit.com>
2025-01-05 21:57:04 +0100

    Saved in TypeScript Playground at 2025-01-05T20:57:03.723Z

--- silos/index.ts
+++ silos/index.ts
@@ -20,18 +20,67 @@ const BUTTON_SIZE = 20; // Size of the buttons ("add" and "remove")
 const BUTTON_GAP = 15; // Gap between the bottom of the buttons and the top of the silos
 const BUTTON_TOP = SILOS_TOP - BUTTON_GAP - BUTTON_SIZE; // Y-coordinate of the top of the buttons
 
-// <<< Add your arrays here
+const silos_x :number[] = []
+
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  // <<< Add your code here
+   let num = 0;
+  for (let i = 0; i < INITIAL_FILL.length; i++) {
+    if (INITIAL_FILL[i] === ",") {
+      silos.push(num);
+      num = 0;
+    } else {
+      num = num * 10 + parseInt(INITIAL_FILL[i].trim());
+    }
+  }
+
+  silos.push(num)
+  for(let i = 0; i<silos.length; i++){
+    silos_x.push(SILO_GAP + i * (SILO_GAP + SILO_WIDTH))
+  }
+
 }
+
 
 function draw() {
   background("black");
 
-  // <<< Add your code here
+  for(let i = 0; i < silos.length;i++){
+    push();
+    translate(silos_x[i], SILOS_TOP)
+     stroke("yellow");
+    strokeWeight(3);
+    line(0,0,0,SILO_HEIGHT)
+    line(SILO_WIDTH,0,SILO_WIDTH,SILO_HEIGHT)
+    line(0,SILO_HEIGHT,SILO_WIDTH,SILO_HEIGHT)
+    pop();
+    push();
+    let ratio = (silos[i] / SILO_MAX) * SILO_HEIGHT;
+    if (ratio > SILO_HEIGHT) {
+      
+    ratio = SILO_HEIGHT;}
+    if(silos[i] > CRICITAL_FILL){
+      fill("red")
+    }
+    else{fill("lime")}
+    translate(silos_x[i], SILOS_TOP); 
+    noStroke()
+    rect(0, SILO_HEIGHT - ratio, SILO_WIDTH, ratio); 
+    pop();
+
+
+
+    push();
+    translate(silos_x[i],  SILOS_TOP + SILO_HEIGHT)
+    noStroke();
+    fill("yellow");
+    textAlign(CENTER, TOP);
+    textSize(20);
+    text(silos[i], SILO_WIDTH / 2, 10);
+    pop();
+  }
 }
 
 function mouseClicked() {
