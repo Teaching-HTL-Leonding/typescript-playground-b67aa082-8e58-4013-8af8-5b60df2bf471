36ce1d983ea54f271242a7f700582569ed62dafe
Rainer Stropek <rainer@timecockpit.com>
2025-01-19 12:20:01 +0100

    Saved in TypeScript Playground at 2025-01-19T11:20:00.611Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -5,7 +5,7 @@ let time = 0
 let timer = 60
 function setup() {
 createCanvas(600,700)
-randomnum = Math.round(random(1,10))
+randomnum = Math.floor(random(0,9))
 squares[randomnum] = true
 }
 
@@ -21,7 +21,21 @@ function draw() {
     line(0,i*200,width,i*200)
     line(i*200,0,i*200,width)}
     pop()
-     
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      let index = row * 3 + col
+      let x = col * 200
+      let y = row * 200
+
+
+      if (squares[index]) {
+        fill("red") // Active square color
+      } else {
+        fill("black") // Inactive square color
+      }
+      rect(x, y, 200, 200)
+    }
+  }
 
 
 
@@ -30,6 +44,15 @@ function draw() {
 
 }
 function mouseClicked(){
+    let col = Math.floor(200/mouseX)
+    let row = Math.floor(200/mouseY)
+    let index = row * 3 + col
 
+
+  if (index ===randomnum) {
+    squares[randomnum] = false // Deactivate the old square
+    randomnum = Math.floor(random(0, 9)) // Choose a new random square
+    squares[randomnum] = true // Activate the new square
+  }
 
 }
