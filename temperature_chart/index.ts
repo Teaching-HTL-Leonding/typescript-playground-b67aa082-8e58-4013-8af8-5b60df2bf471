bbed7e60a626de0b36ed6b85e466d1ed5a2a42db
Rainer Stropek <rainer@timecockpit.com>
2025-01-23 09:32:03 +0100

    Saved in TypeScript Playground at 2025-01-23T08:32:03.203Z

---
+++ temperature_chart/index.ts
@@ -0,0 +1,54 @@
+const AVG_TEMP_LINZ: number[] = [
+  -0.7, 0.5, 4.7, 9.9, 14.2, 17.9, 19.5, 19.3, 14.7, 10, 4.8, 0.6,
+];
+
+const AVG_TEMP_JOHANNESBURG: number[] = [
+  20, 20, 18, 15, 12, 9, 9, 12, 16, 18, 18, 19,
+];
+
+function setup() {
+  createCanvas(460, 300);
+
+  // <<< Call the functions in the following order:
+  // 1. Draw temperatures (gets AVG_TEMP_LINZ or AVG_TEMP_JOHANNESBURG as parameter)
+  // 2. Draw axes
+  // 3. Draw Y labels
+  // 4. Draw X labels
+  drawAxes()
+  drawYLabels()
+  drawXLabels()
+}
+
+function drawAxes() {
+  strokeWeight(3)
+line(50,10,50,210)
+line(50,185,410,185)
+}
+
+function drawYLabels() {
+  let x = 35
+for(let i = 0; i<9; i++){
+  
+  push()
+  translate(50,10)
+  line(-5,i*25,5,i*25)
+  textAlign(RIGHT)
+  text(`${x}`,-10,i*25+4)
+  x -= 5
+  pop()
+}
+}
+
+function drawXLabels() {
+ for(let i = 0; i<13; i++){
+  push()
+translate(50,185)
+line(25*i,-5,25*i,5)
+  pop()
+
+ }
+}
+
+function drawTemperatures(temperatures: number[]) {
+  // <<< Add code to draw the temperatures here
+}
