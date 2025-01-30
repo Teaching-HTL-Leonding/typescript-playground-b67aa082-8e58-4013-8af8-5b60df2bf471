42750d6d56e17b16faa71f6460e911d42980cbaa
Rainer Stropek <rainer@timecockpit.com>
2025-01-30 09:33:07 +0100

    Saved in TypeScript Playground at 2025-01-30T08:33:07.043Z

--- bubbles_with_functions/index.ts
+++ bubbles_with_functions/index.ts
@@ -3,7 +3,7 @@ const circles_x: number[] = [];
 const circles_y: number[] = [];
 const circles_diameter: number[] = [];
 
-let waiting_time = 3000;
+let waiting_time = 30000;
 let circle_interval: number;
 let interval: number;
 
@@ -18,7 +18,7 @@ function setup() {
 
 function draw() {
   background("black");
-
+  isInside()
  for(let i = 0; i<circles_x.length; i++){
 
   circle(circles_x[i],circles_y[i],circles_diameter[i])
@@ -29,4 +29,16 @@ function addRandomCircle(){
   circles_x.push(random(0,width))
   circles_y.push(random(0,height))
   circles_diameter.push(random(10,50))
+}
+
+function isInside(x: number, y: number, circle_index: number){
+  for(let i = 0; i<circles_x.length;i++){
+    push()
+    translate(circles_x[i],circles_y[i])
+    if(Math.sqrt(mouseX^2 +mouseY^2)<=circles_diameter[i])
+    circles_diameter.splice[i]
+    circles_x.splice[i]
+    circles_y.splice[i]
+    pop()
+  }
 }
