3cfb7957d87975024a5681a93111507ebfecc785
Rainer Stropek <rainer@timecockpit.com>
2025-02-01 20:58:08 +0100

    Saved in TypeScript Playground at 2025-02-01T19:58:08.015Z

--- bubbles_with_functions/index.ts
+++ bubbles_with_functions/index.ts
@@ -12,49 +12,48 @@ let points = 0;
 function setup() {
   createCanvas(300, 300);
   addRandomCircle()
-  circle_interval = setInterval(addRandomCircle,waiting_time)
+  circle_interval = setInterval(addRandomCircle, waiting_time)
 
 }
 
 function draw() {
   background("black");
- for(let i = 0; i<circles_x.length; i++){
-  noFill()
-  strokeWeight(2)
-  stroke("white")
-  circle(circles_x[i],circles_y[i],circles_diameter[i])
- }
- textSize(20)
- fill("white")
- noStroke()
- text(points, 10,30)
- if(circles_x.length>= 10){
-  clearInterval(circle_interval)
- }
+  for (let i = 0; i < circles_x.length; i++) {
+    noFill()
+    strokeWeight(2)
+    stroke("white")
+    circle(circles_x[i], circles_y[i], circles_diameter[i])
+  }
+  textSize(20)
+  fill("white")
+  noStroke()
+  text(points, 10, 30)
+  if (circles_x.length >= 10) {
+    clearInterval(circle_interval)
+  }
 }
 
-function addRandomCircle(){
-  circles_x.push(random(0,width))
-  circles_y.push(random(0,height))
-  circles_diameter.push(random(10,50))
+function addRandomCircle() {
+  circles_x.push(random(0, width))
+  circles_y.push(random(0, height))
+  circles_diameter.push(random(10, 50))
 }
 
 function mouseClicked() {
-  for (let i = 0; i <circles_x.length; i++) {
- ;
+  for (let i = 0; i < circles_x.length; i++) {
+    ;
     if (isInside(i)) {
-   
-      circles_x.splice(i,1);
-      circles_y.splice(i,1);
-      circles_diameter.splice(i,1);
+      circles_x.splice(i, 1);
+      circles_y.splice(i, 1);
+      circles_diameter.splice(i, 1);
       points++
-      }
-      }
+    }
+  }
 }
 
 
-function isInside(circleindex:number) :boolean{
+function isInside(circleindex: number): boolean {
 
-const distance = Math.sqrt((mouseX - circles_x[circleindex])**2 + (mouseY - circles_y[circleindex])**2)
-return distance <= circles_diameter[circleindex]/2
+  const distance = Math.sqrt((mouseX - circles_x[circleindex]) ** 2 + (mouseY - circles_y[circleindex]) ** 2)
+  return distance <= circles_diameter[circleindex] / 2
 }
