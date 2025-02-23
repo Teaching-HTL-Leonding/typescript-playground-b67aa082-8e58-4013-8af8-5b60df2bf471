38f662e2d015e224a764161ffde15ab54c7ffa57
Rainer Stropek <rainer@timecockpit.com>
2025-02-23 16:18:00 +0100

    Saved in TypeScript Playground at 2025-02-23T15:17:59.939Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,16 +1,17 @@
+
+const keys = ["W","A","S","D"]
 function setup() {
     createCanvas(500, 500);
+}
+
+function draw() {
     background("black");
     textAlign(CENTER, CENTER);
     textSize(40);
     fill("yellow");
     noStroke();
-}
-
-function keyPressed() {
-    background("black");
-
-    if (key === 'a') {
-        text("You pressed 'a'", width/2, height/2);
+for(let i = 0; i<keys.length;i++){
+    if (keyIsDown(keys[i].charCodeAt(0))) {
+        text(`${keys[i]}`, width/2, height/2);
     }
-}
+}}
