721d3150d06e31327baa6d79d48a52e26e1b5216
Rainer Stropek <rainer@timecockpit.com>
2025-03-04 10:41:53 +0100

    Saved in TypeScript Playground at 2025-03-04T09:41:53.138Z

--- bouncing_ball_advanced/index.ts
+++ bouncing_ball_advanced/index.ts
@@ -1,30 +1,50 @@
-function setup() {
-  createCanvas(300, 200);
-}
-
 const circleDiameter = 50;
 
-let circleCenterX = 200;
-let circleCenterY = 75;
-let xdirection = -2;
-let ydirection = 2;
+// Current center of the ball
+let circleCenterX: number;
+let circleCenterY: number;
+
+// Current direction of the ball in X- and Y-direction
+let directionX = 2;
+let directionY = 2;
+
+function setup() {
+    createCanvas(300, 300);
+    circleCenterX = random(0, width);
+    circleCenterY = random(0, height);
+}
 
 function draw() {
-  background("gold");
+    bounceOfCircle(directionX,directionY)
+    background("black");
+    fill("gold")
+    circle(width/2,height/2,width)
+    stroke("white");
+    strokeWeight(3);
+    fill("lime");
+    circle(circleCenterX, circleCenterY, circleDiameter);
 
-  stroke("white");
-  strokeWeight(3);
-  fill("lime");
-  circle(circleCenterX, circleCenterY, circleDiameter);
+    circleCenterX += directionX;
+    circleCenterY += directionY;
 
-  circleCenterX += xdirection;
-  circleCenterY += ydirection;
-
-  if (circleCenterX >= width || circleCenterX <= 0) {
-    xdirection *= -1;
-  }
-  if (circleCenterY >= height || circleCenterY <= 0) {
-    ydirection *= -1;
-
-  }
+    if (circleCenterX <= circleDiameter/2 || circleCenterX >= width-circleDiameter/2) {
+        directionX *= -1;
+    }
+    if (circleCenterY <= circleDiameter/2 || circleCenterY >= height-circleDiameter/2) {
+        directionY *= -1;
+    }
 }
+    function bounceOfCircle(x:number,y:number){
+      push()
+      translate(width/2,height/2)
+      if(Math.sqrt(x**2+y**2)>= width||Math.sqrt(x**2+y**2)<= width){
+        let num = random(1,2)
+        if(num === 1){
+          directionX *= -1
+        }
+        else if(num === 2){
+          directionY*= -1
+        }
+      }
+      pop()
+    }
