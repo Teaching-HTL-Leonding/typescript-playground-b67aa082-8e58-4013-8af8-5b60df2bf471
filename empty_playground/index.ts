a4c14fa8682b7b477d9eb2c3e29755dd34a14747
Rainer Stropek <rainer@timecockpit.com>
2025-03-04 20:46:43 +0100

    Saved in TypeScript Playground at 2025-03-04T19:46:42.990Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,50 +1,19 @@
-const circleDiameter = 50;
-
-// Aktuelle Position der äußeren Kugel
-let circleCenterX;
-let circleCenterY;
-
-// Bewegungsrichtung
-let directionX = 2;
-let directionY = 2;
+const ones: string[] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine","ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+const tensInWords: string[] = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+let temp:string = prompt("wähle eine zahl zwischen 0-99");
+let num = parseInt(temp)
 
 function setup() {
-    createCanvas(300, 300);
-    circleCenterX = random(circleDiameter / 2, width - circleDiameter / 2);
-    circleCenterY = random(circleDiameter / 2, height - circleDiameter / 2);
+    if(num<20){
+       console.log(ones[num])
+    }
+    if(num>=20){
+        let result =""
+        result += tensInWords[parseInt(temp[0])-2]
+        result += ones[parseInt(temp[1])]
+        console.log(result)
+    }
 }
 
 function draw() {
-    background("black");
-    
-    // Zeichne die große zentrale Kugel
-    fill("gold");
-    noStroke();
-    circle(width / 2, height / 2, width);
-    
-    // Zeichne die bewegliche äußere Kugel
-    stroke("white");
-    strokeWeight(3);
-    fill("lime");
-    circle(circleCenterX, circleCenterY, circleDiameter);
-    
-    // Bewegung
-    circleCenterX += directionX;
-    circleCenterY += directionY;
-    
-    // Prüfe Kollision mit der großen Kugel
-    bounceOffInnerCircle();
-}
-
-function bounceOffInnerCircle() {
-    let distX = circleCenterX - width / 2;
-    let distY = circleCenterY - height / 2;
-    let distance = sqrt(distX * distX + distY * distY);
-    let bigCircleRadius = width / 2 - circleDiameter / 2;
-
-    if (distance >= bigCircleRadius) {
-        let angle = atan2(distY, distX);
-        directionX = cos(angle) * -2;
-        directionY = sin(angle) * -2;
-    }
 }
