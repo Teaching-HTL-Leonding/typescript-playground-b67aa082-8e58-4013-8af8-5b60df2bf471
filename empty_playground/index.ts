636d9ce51e4c5d9b9ed323a1d83331ce3b8460a6
Rainer Stropek <rainer@timecockpit.com>
2025-03-04 10:55:35 +0100

    Saved in TypeScript Playground at 2025-03-04T09:55:34.898Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,14 +1,50 @@
-let numbers = [1,4,5,6]
-let target = 9
-console.log(numberGuesser(numbers,target))
-function numberGuesser(nums: number[] , target: number):any[]{
+const circleDiameter = 50;
 
-for(let i = 0; i<nums.length; i++){
-    for(let k = i+1; k<nums.length;k++){
-        if(nums[k] + nums[i] === target){
-            return [nums[i],nums[k]]
-        }
-    }
+// Aktuelle Position der äußeren Kugel
+let circleCenterX;
+let circleCenterY;
+
+// Bewegungsrichtung
+let directionX = 2;
+let directionY = 2;
+
+function setup() {
+    createCanvas(300, 300);
+    circleCenterX = random(circleDiameter / 2, width - circleDiameter / 2);
+    circleCenterY = random(circleDiameter / 2, height - circleDiameter / 2);
 }
-return [`in this array there are no two elements that equal ${target}`]
+
+function draw() {
+    background("black");
+    
+    // Zeichne die große zentrale Kugel
+    fill("gold");
+    noStroke();
+    circle(width / 2, height / 2, width);
+    
+    // Zeichne die bewegliche äußere Kugel
+    stroke("white");
+    strokeWeight(3);
+    fill("lime");
+    circle(circleCenterX, circleCenterY, circleDiameter);
+    
+    // Bewegung
+    circleCenterX += directionX;
+    circleCenterY += directionY;
+    
+    // Prüfe Kollision mit der großen Kugel
+    bounceOffInnerCircle();
+}
+
+function bounceOffInnerCircle() {
+    let distX = circleCenterX - width / 2;
+    let distY = circleCenterY - height / 2;
+    let distance = sqrt(distX * distX + distY * distY);
+    let bigCircleRadius = width / 2 - circleDiameter / 2;
+
+    if (distance >= bigCircleRadius) {
+        let angle = atan2(distY, distX);
+        directionX = cos(angle) * -2;
+        directionY = sin(angle) * -2;
+    }
 }
