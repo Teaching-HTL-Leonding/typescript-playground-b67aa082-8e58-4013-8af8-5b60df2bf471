773a8171a4aedb4f1b75ab5a2b22bc5510e8a4ee
Rainer Stropek <rainer@timecockpit.com>
2025-03-30 10:41:45 +0000

    Saved in TypeScript Playground at 2025-03-30T10:41:44.760Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,144 +1,82 @@
-    const unitSize = 25
-    let running = false;
-    let score = 0
-    let xvelocity = unitSize;
-    let yvelocity = 0;
-    let snake = [
-        {x:unitSize*4, y:0},
-        {x:unitSize*3, y:0},
-        {x:unitSize*2, y:0},
-        {x:unitSize*1, y:0},
-        {x:unitSize*0, y:0}
+const wordtoguess = "abcde"
+let rows = 0
+const UNIT = 35
+let userinput: string[] = ["", "", "", "", "", ""]
+const allowedkeys = "abcdefghijklmnopqrstuvwxyz"
 
-    ]
-    let randx = 0
-    let randy = 0
-    window.addEventListener("keydown",changeDirection)
 function setup() {
-    createCanvas(500,500)
-    background("white")
-    gameStart();
+    createCanvas(235, 250)
+    background("black")
+    drawOutlines();
 }
 
 
-
-function gameStart(){
-    running = true;
-    createFood();
-    nextTick();
-};
-function nextTick(){
-    if(running){
-        setTimeout(()=>{
-            clearBoard();
-            drawFood();
-            moveSnake();
-            drawSnake();
-            checkGameOver();
-            nextTick();
-            fill("black")
-            textSize(20)
-            text(score,width-50,50)
-        },75)
+function drawOutlines() {
+    stroke("white")
+    push();
+    translate(UNIT, UNIT)
+    for (let i = 0; i < 7; i++) {
+        line(0, UNIT * i, width - UNIT * 2 + 10, UNIT * i)
     }
-    else{
-        displayGameOver()
+    for (let i = 0; i < 6; i++) {
+        line(UNIT * i, 0, UNIT * i, width + 10 - UNIT)
     }
-};
-function clearBoard(){
-    background("white")
-    noFill();
-    strokeWeight(3)
-    rect(0,0,width,height)
-};
-function createFood(){
-    randx = Math.floor(random(0,width)/unitSize)*unitSize
-    randy = Math.floor(random(0,height)/unitSize)*unitSize
-
+    pop();
 }
-function moveSnake(){
-    const head = {x: snake[0].x+xvelocity,
-                  y: snake[0].y+yvelocity}
-                  snake.unshift(head)
-                  if(snake[0].x == randx && snake[0].y == randy){
-                        score++
-                        createFood();
-                  }
-                  else{
-                    snake.pop();
-                  }
-};
-function drawSnake(){
-    fill("green")
-    for(const snakepart of snake){
-        rect(snakepart.x,snakepart.y,unitSize,unitSize)
+function keyPressed() {
+    background("black")
+
+    for (let i = 0; i < userinput.length; i++) {
+        for (let j = 0; j < userinput[i].length; j++)
+            if (userinput[i].length < 5 && allowedkeys.includes(userinput[i][j])) {
+
+
+                userinput[i] += key;
+                break; // Sobald ein Zeichen eingefügt wurde, stoppen wir die Schleife
+            }
+
     }
-    
-    
-};
-function drawFood(){
-        fill("red")
-    rect(randx,randy,unitSize)
+    drawOutlines();
+    drawGuesses(userinput);
+    checkIfWon(userinput)
 }
-function changeDirection(event){
-    const keypressed = event.keyCode
-    const LEFT = 65
-    const RIGHT = 68
-    const UP = 87
-    const DOWN = 83
-    const goingUP = (yvelocity == -unitSize)
-    const goingDOWN = (yvelocity == unitSize)
-    const goingRIGHT = (xvelocity == unitSize)
-    const goingLEFT = (xvelocity == -unitSize)
-    switch(true){
-        case(keypressed == LEFT&&!goingRIGHT):
-        xvelocity = -unitSize
-        yvelocity = 0;
-        break;
-        case (keypressed == UP&&!goingDOWN):
-        xvelocity= 0;
-        yvelocity= -unitSize
-        break;
-                case (keypressed == RIGHT&&!goingLEFT):
-        xvelocity= unitSize;
-        yvelocity= 0
-        break;
-                case (keypressed == DOWN&&!goingUP):
-        xvelocity= 0;
-        yvelocity= unitSize
-        break;
-    }
-
-
-   
-
-};
-function checkGameOver(){
-    switch(true){
-        case (snake[0].x < 0):
-        running = false;
-        break;
-            case (snake[0].x >= width):
-        running = false;
-        break;
-                case (snake[0].y < 0):
-        running = false;
-        break;
-                case (snake[0].y >= height):
-        running = false;
-        break;
-    }
-    for(let i = 0; i<snake.length;i++){
-        if(snake[i].x== snake[0].x&&snake[i].y== snake[0].y){
-      
+function drawGuesses(userinput: string[]) {
+    push();
+    translate(UNIT, UNIT)
+    for (let i = 0; i < userinput.length; i++) {
+        for (let j = 0; j < userinput[i].length; j++) {
+            if (wordtoguess[j] === userinput[i][j]) {
+                fill("green");
+                noStroke();
+            }
+            else if (wordtoguess.includes(userinput[i][j])) {
+                fill("orange");
+                noStroke();
+            }
+            else {
+                fill("white");
+                noStroke();
+            }
+            text(userinput[i][j], 15 + UNIT * j, 15 + UNIT * i)
         }
     }
-    };
-function displayGameOver(){
-    fill("black")
-    textAlign(CENTER)
-    textSize(20)
-    text(`GAME OVER! You reached ${score} points`,width/2,height/2)
-};
-
-
+    pop();
+}
+function checkIfWon(userinput: string[]) {
+    switch (true) {
+        case (userinput[1] === ""): rows = 1; break;
+        case (userinput[2] === ""): rows = 2; break;
+        case (userinput[3] === ""): rows = 3; break;
+        case (userinput[4] === ""): rows = 4; break;
+        case (userinput[5] === ""): rows = 5; break;
+        case (userinput[6] === ""): rows = 6; break;
+    }
+    for (let i = 0; i < userinput.length; i++) {
+        if (userinput[i] === wordtoguess) {
+            background("black")
+            textSize(20)
+            fill("white")
+            text(`you won with ${6 - rows} rows left`, 0, height / 2)
+        }
+    }
+}
