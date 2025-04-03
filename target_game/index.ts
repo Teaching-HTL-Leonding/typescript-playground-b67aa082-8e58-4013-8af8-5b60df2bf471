82c66d7b49eeb2986c4631d74ec1226646a043e8
Rainer Stropek <rainer@timecockpit.com>
2025-04-03 07:01:03 +0000

    Saved in TypeScript Playground at 2025-04-03T07:01:03.125Z

--- target_game/index.ts
+++ target_game/index.ts
@@ -1,27 +1,27 @@
 // <<< Add your game variables here
 let dragging = false;
-const smallradius = 50;
-const largeradius = 70;
-let smallcirclex= 0
-let smallcircley= 0
+const smallradius = 200;
+const largeradius = 220;
+let smallcirclex = 0
+let smallcircley = 0
 let largecirclex = 0
 let largecircley = 0
 let interval;
 let score = 0;
-function setup() {  
-createCanvas(800, 600);
-smallcirclex = findRandomXPos(smallradius);
-smallcircley = findRandomYPos(smallradius);
-largecirclex = findRandomXPos(largeradius);
-largecircley = findRandomYPos(largeradius);
+function setup() {
+  createCanvas(800, 600);
+  smallcirclex = findRandomXPos(smallradius);
+  smallcircley = findRandomYPos(smallradius);
+  largecirclex = findRandomXPos(largeradius);
+  largecircley = findRandomYPos(largeradius);
 
-while(circleInCircle(smallcirclex,smallcircley,smallradius,largecirclex,largecircley,largeradius)){
-smallcirclex = findRandomXPos(smallradius);
-smallcircley = findRandomYPos(smallradius);
-largecirclex = findRandomXPos(largeradius);
-largecircley = findRandomYPos(largeradius);
-}
-interval = setInterval(endGame,60000)
+  while (circleInCircle(smallcirclex, smallcircley, smallradius, largecirclex, largecircley, largeradius)) {
+    smallcirclex = findRandomXPos(smallradius);
+    smallcircley = findRandomYPos(smallradius);
+    largecirclex = findRandomXPos(largeradius);
+    largecircley = findRandomYPos(largeradius);
+  }
+  interval = setInterval(endGame,60000)
 
   // <<< Add setup logic here
 }
@@ -30,66 +30,67 @@ function draw() {
   background("lightgray");
   fill("lightblue")
 
-  circle(smallcirclex,smallcircley,smallradius * 2)
+  circle(smallcirclex, smallcircley, smallradius * 2)
   noFill();
-  circle(largecirclex,largecircley,largeradius* 2)
+  circle(largecirclex, largecircley, largeradius * 2)
   fill("black")
-  text(`score:${score}`,20,height-10)
+  text(`score:${score}`, 20, height - 10)
 }
 
 function mousePressed() {
-  dragging = isInRadius(mouseX,mouseY,smallcirclex,smallcircley)
+  dragging = isInRadius(mouseX, mouseY, smallcirclex, smallcircley)
 }
 
 function mouseDragged() {
-  if(dragging){
+  if (dragging) {
     smallcirclex = mouseX
     smallcircley = mouseY
 
-    
+
   }
-  
+
   // <<< Add mouse dragged logic here
 }
 
 function mouseReleased() {
   dragging = false
-  if(circleInCircle(smallcirclex,smallcircley,smallradius,largecirclex,largecircley,largeradius)){
-    while(circleInCircle(smallcirclex,smallcircley,smallradius,largecirclex,largecircley,largeradius)){
-  largecirclex = findRandomXPos(largeradius);
-  largecircley = findRandomYPos(largeradius);
-    score++
+  if (circleInCircle(smallcirclex, smallcircley, smallradius, largecirclex, largecircley, largeradius)) {
+    while (circleInCircle(smallcirclex, smallcircley, smallradius, largecirclex, largecircley, largeradius)) {
+      largecirclex = findRandomXPos(largeradius);
+      largecircley = findRandomYPos(largeradius);
+      score++
+    }
   }
 }
-}
 
 
-function isInRadius(mx:number,my:number,x:number,y:number):boolean{
-  const dx = mx - x  
+function isInRadius(mx: number, my: number, x: number, y: number): boolean {
+  const dx = mx - x
   const dy = my - y
-  const distance = Math.sqrt(dx*dx+dy*dy)
- 
-  return distance <  smallradius
+  const distance = Math.sqrt(dx * dx + dy * dy)
+
+  return distance < smallradius
 }
-function findRandomXPos(radius:number):number{
-  const randomX = random(radius,width-radius)
+function findRandomXPos(radius: number): number {
+  const randomX = random(radius, width - radius)
   return randomX
 
 }
-function findRandomYPos(radius:number):number{
-  const randomY = random(radius,height-radius)
+function findRandomYPos(radius: number): number {
+  const randomY = random(radius, height - radius)
   return randomY
 }
-function circleInCircle(x1:number,y1:number,r1:number,x2:number,y2:number,r2:number):boolean{
-    const dx = x1 - x2
-    const dy = y1 - y2
-  const distance = Math.sqrt(dx*dx+dy*dy)
-  return distance + r1 < r2
-  
-}
-function endGame(){
+function circleInCircle(x1: number, y1: number, r1: number, x2: number, y2: number, r2: number): boolean {
+  const dx = x1 - x2
+  const dy = y1 - y2
+  const distance = Math.sqrt(dx * dx + dy * dy)
+  return distance <= r1+ r2
 
- clearInterval(interval);
- noLoop();
- background("lightgray")
+}
+function endGame() {
+
+  clearInterval(interval);
+  noLoop();
+  background("lightgray")
+  text(`Game Over!\n Final Score: ${score}`, width / 2, height / 2)
 }
