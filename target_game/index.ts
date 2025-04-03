020dd124bc85471e831100f6b3c3b9591b147802
Rainer Stropek <rainer@timecockpit.com>
2025-04-03 07:29:19 +0000

    Saved in TypeScript Playground at 2025-04-03T07:29:19.320Z

--- target_game/index.ts
+++ target_game/index.ts
@@ -97,7 +97,7 @@ function circleTouchesCircle(x1:number,y1:number,r1:number,x2:number,y2:number,r
       const dx = x1 - x2
       const dy = y1 - y2
       const distance = Math.sqrt(dx*dx+dy*dy)
-      return distance > r1 + r2
+      return distance < r1 + r2
 }
 function endGame(){
  clearInterval(interval);
